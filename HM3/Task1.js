959d26b4b2dc36314b24ed28126e3495980e16f7
Mikita <nikitarakovich61@gmail.com>
2024-04-13 22:40:59 +0200

    hm3 task1, task2 done

---
+++ HM3/Task1.js
@@ -0,0 +1,32 @@
+const Products = [
+    {name: "Bread", price: 3},       
+    {name: "Milk", price: 4.5},
+    {name: "Eggs", price: 5.9},
+    {name: "Juice", price: 3.4},
+    {name: "Pasta", price: 4},
+    {name: "Apples", price: 2.4},
+    {name: "Bacon", price: 5.6},
+    {name: "Beer", price: 3.5},
+    {name: "Avocado", price: 8}
+];
+
+function calculateDiscountedPrice(products, discount){
+    let discounted = [];
+    products.forEach(product => {
+        discounted.push({name: product.name, price: product.price - (product.price * discount / 100)});
+    });
+    return discounted;
+}
+
+function calculateTotalPrice(products){
+    total = 0;
+    products.forEach(product =>{
+        total += product.price;
+    });
+    return total;
+}
+
+console.log(calculateDiscountedPrice(Products, 30));
+console.log(calculateTotalPrice(Products));
+console.log(calculateTotalPrice(calculateDiscountedPrice(Products, 50)));
+console.log(Products);
---
+++ HM3/Task2.js
@@ -0,0 +1,49 @@
+const persons =[
+    {firstName: "Mikita", lastName: "Rakovich",},
+    {firstName: "Jakub", lastName: "Kowal"},
+    {firstName: "John", lastName: "Cena"}
+];
+
+const getFullName = person => `${person.firstName} ${person.lastName}`;
+console.log(persons.map(getFullName));
+///////////////////
+
+
+const findUnique = (regex) => (str) => {
+    return str.replace(regex, '').split(/\s+/);
+};
+
+const alphabeticOrder = (words) => {
+    return words.sort();
+};
+
+const pipe = (...funcs) => (value) => {
+    return funcs.reduce((currentValue, currentFunc) => {
+        return currentFunc(currentValue);
+    }, value);
+};
+
+const filterUniqueWords = pipe(findUnique(/\b(\w+)\b(?=.*\b(\1)\b)/g), alphabeticOrder);
+
+console.log(filterUniqueWords("banana apple zombie apple air grape grape string"));
+//////////////////
+
+const getGrades = (students) => students.map(student => student.grade);
+
+const calculateAverage = (grades) => {
+    const sum = grades.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return sum / grades.length;
+};
+
+
+const getAverageGrade = pipe(getGrades, calculateAverage);
+
+const students = [
+    { name: "Mikita", grade: 4 },
+    { name: "Jakub", grade: 5 },
+    { name: "John", grade: 3.5 }
+];
+
+console.log(getAverageGrade(students)); // Output: 4.166666666666667
+
+
